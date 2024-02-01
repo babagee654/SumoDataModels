48b2b545b3e9350b6b2d0f97502cae411c4c8034
Jerome Acosta <gerome654@hotmail.com>
2024-02-01 14:20:33 -0500

    Added SumoDBPages

--- src/Enums.ts
+++ src/Enums.ts
@@ -17,3 +17,10 @@ export enum Divisions {
     Makuuchi = "Makuuchi",
     Juryo = "Juryo",
 }
+
+export enum SumoDBPages {
+    Rikishi = "https://sumodb.sumogames.de/Rikishi.aspx", // ?r=1197 for specific rikishi
+    Basho = "https://sumodb.sumogames.de/Banzuke.aspx", // for latest Basho Banzuke
+    RikishiBasho = "https://sumodb.sumogames.de/Rikishi_basho.aspx", // ?r=11927&b=202401 for specific rikishi basho record
+    BashoResultsByDay = "https://sumodb.sumogames.de/Results.aspx", // ?b=202401&d=1 for specific basho day
+}
